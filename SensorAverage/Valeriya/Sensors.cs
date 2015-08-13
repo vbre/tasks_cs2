69226538165923ae1d7a9e107e46c9330174333c
vbre <angietheowl@gmail.com>
2015-08-13 19:37:25 +0300

    Begin of task
    Parsed Sensors value

---
+++ SensorAverage/Valeriya/Sensors.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SensorAverage.Valeriya
+{
+
+    class Sensors
+    {
+        static int countOfSensors = 3;
+        static List<double> values = new List<double>();
+        static double[] codes = new double[Sensors.countOfSensors];
+        static List<double> ParseSensorsData (ushort[] data)
+        {
+            foreach(ushort element in data)
+            {
+                codes[element] = data[element] >> 13;
+                Sensors.values.Add(data[element] >> 3);
+            }
+
+            return Sensors.values;
+        }
+    }
+}

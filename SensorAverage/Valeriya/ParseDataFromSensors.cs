ab01b4203f8efdcfb3374bfd0e92d636cd759b60
vbre <angietheowl@gmail.com>
2015-08-17 00:26:23 +0300

    Almost done
    values sort by code of sensor is missing

--- SensorAverage/Valeriya/ParseDataFromSensors.cs
+++ SensorAverage/Valeriya/ParseDataFromSensors.cs
@@ -6,8 +6,36 @@ using System.Threading.Tasks;
 
 namespace SensorAverage.Valeriya
 {
-    class ParseDataFromSensors
+    public class ParseDataFromSensors
     {
+        public struct AverageBySensor
+        {
+           public ushort code;
+           public double value;
+           
+           public AverageBySensor (ushort code, double value)
+            {
+                this.code = code;
+                this.value = value;
+            }
+        }
 
+        public List<AverageBySensor> ParseData (ushort[] data)
+        {
+            List<AverageBySensor> dataFromSensors = new List<AverageBySensor>();
+            //AverageBySensor dataFromSensor = new AverageBySensor();            
+            for (int i = 0; i < data.Length; i++)
+            {
+                ushort codeOfSensor = (ushort)(data[i] >> 13);
+                if ((data[i] & 0x1) == 1)
+                {
+                    var temp = data[i] << 3;
+                    double valueFromSensor = temp >> 4;
+                    dataFromSensors.Add(new AverageBySensor(codeOfSensor, valueFromSensor));
+                }
+            }
+
+            return dataFromSensors;
+        }
     }
 }
--- SensorAverage/Valeriya/SensorAverage.cs
+++ SensorAverage/Valeriya/SensorAverage.cs
@@ -8,31 +8,20 @@ namespace SensorAverage.Valeriya
 {
     class SensorAverage : ISensorsAverage
     {
-        
+        ParseDataFromSensors dataParser = new ParseDataFromSensors();
         public Tuple<ushort, double>[] GetAverages(ushort[] data)
         {
-            Tuple<ushort, double>[] dataFromSensor = new Tuple<ushort, double>[data.Length];
+            int numberOfSensors = 3;
+            double sumForSensor1 = 0;
+
+            Tuple<ushort, double>[] averageFromSensors = new Tuple<ushort, double>[numberOfSensors];
             for (int i = 0; i < data.Length; i++)
             {
-                double valueFromSensor = data[i] >> 13;
-                var temp = data[i] << 3;
-                ushort codeOfSensor = (ushort)(temp >> 4);
-                dataFromSensor[i] = Tuple.Create(codeOfSensor, valueFromSensor);
+                sumForSensor1 += averageFromSensors[i].Item2;
             }
 
-            double sumForSensor1 = 0;
-            for (int i = 0; i < dataFromSensor.Length; i++)
-            {
-                if (dataFromSensor[i].Item1 == dataFromSensor[i+1].Item1)
-                {
-                    sumForSensor1 = sumForSensor1 + dataFromSensor[i].Item1;
-                    ushort sensorFist = dataFromSensor[i].Item1;
+           return averageFromSensors;
 
-                }
-                
-            }
-
-            return dataFromSensor;
         }
     }
 }

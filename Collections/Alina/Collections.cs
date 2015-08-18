06f02ed221ced11f808e3452927369d45bd66461
AlinaKulishdp <kulis.alina.dp@yandex.ru>
2015-08-18 22:10:08 +0300

    Sort potatoes

---
+++ Collections/Alina/Collections.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collections.Alina
+{
+    class Collections: ICollections
+    {
+        public int SortPotatoes(List<IPotatoe> potatoeBag, out List<IPotatoe> goodPotatoes, out List<IPotatoe> badPotatoes)
+        {
+            List<IPotatoe> good = new List<IPotatoe>();
+            List<IPotatoe> bad = new List<IPotatoe>();
+            foreach (var potatoe in potatoeBag)
+            {
+                if (potatoe.IsBad)
+                {
+                    bad.Add(potatoe);
+                }
+                else
+                {
+                    good.Add(potatoe);
+                }
+                potatoeBag.Remove(potatoe);
+            }
+            goodPotatoes = good;
+            badPotatoes = bad;
+            return good.Count;
+        }
+    }
+}

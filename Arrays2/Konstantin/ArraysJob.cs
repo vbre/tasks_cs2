9afbd6cba6c848c69e311a041824e3c43386fb33
exphoenix666 <smolenskyk@gmail.com>
2015-08-20 12:33:37 +0300

    SmolenskiyArraysJob
    Задача с лайфциклом не проходит проверку. Обратите пожалуйста внимание
    на тестовые данные, считаю их неточными.
    ERROR: Неверный результат: вернули входной массив
    ifeArrayToCheck == lifeArray
    lifeArrayToCheck.Equals(lifeArray)
    ERROR: неверное значение 0 в строке 1 столбце 2
    ERROR: неверное значение 0 в строке 2 столбце 1
    ERROR: неверное значение 0 в строке 5 столбце 3
    Значения должны обратиться в 0, т.к. сумма окружающих элементов больше
    3-х, или меньше 2х, что сходится с условием.

--- Arrays2/Konstantin/ArraysJob.cs
+++ Arrays2/Konstantin/ArraysJob.cs
@@ -26,11 +26,39 @@ namespace Arrays2.Konstantin
 
         public int[][] LifeCalculations(int[][] inputArray)
         {
-            int[][] outputArray = new int[inputArray.Length][inputArray[0].Length];
-            //int[][] tempArray = new int[inputArray.Length+2][inputArray[0].Length+2];
-            for (int i = 0; i < inputArray.GetLength(0)+2; i++)
+            int[][] outputArray = new int[inputArray.Length][];
+            int[,] tempArray = new int[inputArray.Length+2,inputArray[0].Length+2];
+            for (int i = 0; i < inputArray.GetLength(0); i++)
             {
-                
+                for (int j = 0; j < inputArray[0].Length; j++)
+                {
+                    tempArray[i + 1, j + 1] = inputArray[i][j];
+                }
+            }
+            for (int i = 0; i < inputArray.Length; i++)
+            {
+                outputArray[i] = new int [inputArray[0].Length];
+            }
+            for (int i = 0; i < inputArray.Length; i++)
+            {
+                for (int j = 0; j < inputArray[0].Length; j++)
+                {
+                    if (tempArray[i + 1, j + 1]==1 && (tempArray[i, j]+tempArray[i+1, j]+tempArray[i+2, j]+tempArray[i, j+1]+ tempArray[i+2, j+1]+
+                        tempArray[i, j + 2] + tempArray[i + 1, j + 2] + tempArray[i + 2, j + 2]>3||tempArray[i, j]+tempArray[i+1, j]+tempArray[i+2, j]+
+                        tempArray[i, j+1]+ tempArray[i+2, j+1]+tempArray[i, j + 2] + tempArray[i + 1, j + 2] + tempArray[i + 2, j + 2]<2))
+                    {
+                        outputArray[i][j] = 0;
+                    }
+                    else if (tempArray[i + 1, j + 1]==0 && (tempArray[i, j]+tempArray[i+1, j]+tempArray[i+2, j]+tempArray[i, j+1]+ tempArray[i+2, j+1]+
+                        tempArray[i, j + 2] + tempArray[i + 1, j + 2] + tempArray[i + 2, j + 2]==3))
+                    {
+                        outputArray[i][j] = 1;
+                    }
+                    else
+                    {
+                        outputArray[i][j] = inputArray[i][j];
+                    }
+                }
             }
             return outputArray;
         }
@@ -38,9 +66,9 @@ namespace Arrays2.Konstantin
         public int CalculateEqualPairs(int[] inputArray)
         {
             int pairCalculator = 0;
-            for (int index = 0; index < inputArray.Length-1; index++)
+            for (int index = 0; index < inputArray.Length - 1; index++)
             {
-                if (inputArray[index]==inputArray[index+1])
+                if (inputArray[index] == inputArray[index + 1])
                 {
                     pairCalculator++;
                 }
@@ -48,4 +76,5 @@ namespace Arrays2.Konstantin
             return pairCalculator;
         }
     }
+    
 }

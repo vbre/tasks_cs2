2740c673371c2fda5a1c6c53f5613d8ce1fd81e6
AlinaKulishdp <kulis.alina.dp@yandex.ru>
2015-09-06 20:11:01 +0300

    Class PriorityQueue is implemented

---
+++ Encapsulation/Alina/PriorityQueue.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulation.Alina
+{
+    class PriorityQueue <T> : IPriorityQueue<T>
+    {
+        SortedDictionary<int, Queue<T>> myQueue = new SortedDictionary<int, Queue<T>>();
+        public void Enqueue(T val, int priority)
+        {
+            if (!myQueue.ContainsKey(priority))
+            {
+                myQueue.Add(priority, new Queue<T>());
+            }
+            myQueue[priority].Enqueue(val);
+        }
+
+        public T Dequeue()
+        {
+            return myQueue.First().Value.Dequeue();
+        }
+
+        public T First()
+        {
+            return myQueue.First().Value.Peek();
+        }
+
+        public T First(int priority)
+        {
+            return myQueue[priority].Peek();
+        }
+
+        public T Last()
+        {
+            return myQueue.Last().Value.ElementAt(myQueue.Last().Value.Count-1);
+        }
+
+        public T Last(int priority)
+        {
+            return myQueue[priority].Last();
+        }
+
+        public int Count
+        {
+            get { return myQueue.Values.Sum(q => q.Count); }
+        }
+
+        public int GetCount(int priority)
+        {
+           return myQueue[priority].Count;
+        }
+    }
+}

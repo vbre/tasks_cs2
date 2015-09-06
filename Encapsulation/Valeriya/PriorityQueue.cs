7dcf6e10da35b7a8a6d466f0f00699edfc7deae2
Valery Brezhnieva <angietheowl@gmail.com>
2015-09-06 13:29:43 +0300

    OnWork #2

--- Encapsulation/Valeriya/PriorityQueue.cs
+++ Encapsulation/Valeriya/PriorityQueue.cs
@@ -6,7 +6,50 @@ using System.Threading.Tasks;
 
 namespace Encapsulation.Valeriya
 {
-    class PriorityQueue
+    class PriorityQueue : Encapsulation.IPriorityQueue <T>
     {
+        List<Tuple<int, T>> priorityQueue = new List<Tuple<int, T>>;
+        public int Count
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public T Dequeue()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Enqueue(T val, int priority)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T First()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T First(int priority)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetCount(int priority)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Last()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Last(int priority)
+        {
+            throw new NotImplementedException();
+        }
     }
 }

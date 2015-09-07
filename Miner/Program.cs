daf1f3af1f7d8e4196a8a4deba12e6b713ca0529
Mechtatel-student <Mechtatel._T@mail.ru>
2015-09-07 18:31:52 +0300

    Program. cs fixed

--- Miner/Program.cs
+++ Miner/Program.cs
@@ -11,12 +11,12 @@ namespace Miner
         static void Main(string[] args)
         {
             List<IMinerGameFactory> factories = new List<IMinerGameFactory>();
-            //factories.Add(new Alina.MinerGameFactory());
-            //factories.Add(new Andrey.MinerGameFactory());
-            //factories.Add(new Elena.MinerGameFactory());
-            //factories.Add(new Konstantin.MinerGameFactory());
+            factories.Add(new Alina.MinerGameFactory());
+            factories.Add(new Andrey.MinerGameFactory());
+            factories.Add(new Elena.MinerGameFactory());
+            factories.Add(new Konstantin.MinerGameFactory());
             factories.Add(new Valeriya.MinerGameFactory());
-            //factories.Add(new Vladimir.MinerGameFactory());
+            factories.Add(new Vladimir.MinerGameFactory());
 
             if (args.Length > 0)
             {

490cb8ce58422e10b376a3033bff604eaed92637
AlinaKulishdp <kulis.alina.dp@yandex.ru>
2015-09-08 23:37:44 +0300

    Used System.Collections.ObjectModel

---
+++ Encapsulation/Alina/MyDictionary.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulation.Alina
+{
+    public class MyDictionary : KeyedCollection <Tuple <string, string, int>, Student>
+    {
+        protected override Tuple<string, string, int> GetKeyForItem(Student item)
+        {
+            Tuple<string, string, int> myKey = new Tuple<string, string, int>(item.FirstName, item.LastName, item.PersonalCode);
+            return myKey;
+        }
+    }
+}

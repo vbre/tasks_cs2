31ba048a4c62314dc286a01e6a30777a2a1521bd
exphoenix666 <smolenskyk@gmail.com>
2015-09-15 00:51:58 +0300

    AnimalClinic Smolenskiy

---
+++ VeterinaryKonstantin/KindOfAnimal/Dog.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VeterinaryKonstantin
+{
+    class Dog : AnimalInClinic
+    {
+        public Dog() : base() { }
+        public Dog(string name, int birthYear, double weight, string anamnesis) : base(name, birthYear, weight,anamnesis) { } 
+        public override void ExaminationReaction()
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Dog can bite the doctor");
+            Console.ResetColor();
+        }
+    }
+}
--- VeterinaryKonstantin/Program.cs
+++ VeterinaryKonstantin/Program.cs
@@ -10,6 +10,39 @@ namespace VeterinaryKonstantin
     {
         static void Main(string[] args)
         {
-        }
+            IVeterinary veterenary = new Veterinary();
+            string menuChoice = string.Empty;
+
+            Console.WriteLine("[H] About company\n[R] Put new animal in clinic\n[L] Show list of animals\n[D] Discharge animal \n[E] Examination in clinic \n[Q] Quit\n");
+
+            do
+            {
+                Console.WriteLine("Enter your choice please, it must be H, R, L, D, E or Q");
+                menuChoice = (Console.ReadLine()).ToUpper();
+                
+                switch (menuChoice)
+                {
+                    case "H":
+                        veterenary.AboutCompany();
+                        break;
+                    case "R":
+                        veterenary.PutANewAnimalInClinic();
+                        break;
+                    case "L":
+                        veterenary.ShowListOfAnimals();
+                        break;
+                    case "D":
+                        veterenary.DischargeAnimal();
+                        break;
+                    case "E":
+                        veterenary.ExaminationInClinic();
+                        break;
+                    default:
+                        break;
+                }
+            } while (menuChoice!="Q");
+
+            Console.ReadKey(true);
+        }        
     }
 }

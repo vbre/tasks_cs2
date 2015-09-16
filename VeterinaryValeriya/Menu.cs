07c58a50503e0664dd203bb724d880da6350cb59
Valery Brezhnieva <angietheowl@gmail.com>
2015-09-16 23:31:54 +0300

    Onwork

---
+++ VeterinaryValeriya/Menu.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VeterinaryValeriya
+{
+    class Menu
+    {
+        public struct MenuItem
+        {
+            public char Code;
+            public string Label;
+            public Action ActionToDo;
+        }
+
+        private List<MenuItem> items;
+
+        public Menu (List<MenuItem> ListOfMenuItems)
+        {
+            foreach (MenuItem elem in ListOfMenuItems)
+            {
+                items.Add(elem);
+            }
+        }
+
+        private void PrintMenu ()
+        {
+            foreach (MenuItem elem in items)
+            {
+                Console.WriteLine("{0} - {1}", elem.Code, elem.Label);
+            }
+        }
+
+        public void HandleUserInput (char code)
+        {
+            Action currentAction = null;
+            do
+            {
+                PrintMenu();
+                int currentItemIndex = items.FindIndex((items) => { return items.Code == code; });
+                if (currentItemIndex != -1)
+                    currentAction = items[currentItemIndex].ActionToDo;
+                if (currentAction != null)
+                {
+                    currentAction();
+                }
+            }
+            while (currentAction != null);
+        }
+    }
+}
--- VeterinaryValeriya/Program.cs
+++ VeterinaryValeriya/Program.cs
@@ -16,71 +16,37 @@ namespace VeterinaryValeriya
 
     class Program
     {
+        static ClinicRegistry AnimalRegistry = new ClinicRegistry();
+
         static void Main(string[] args)
         {
-            ClinicRegistry AnimalRegistry = new ClinicRegistry();
+            Menu mainMenu = new Menu(
+            new List<Menu.MenuItem> {
+               new Menu.MenuItem { Label = "About company", ActionToDo = ShowAboutCompany },
+               new Menu.MenuItem { Label = "Put new animal in clinic", ActionToDo = PutAnimalInClinic },
+               new Menu.MenuItem { Label = "Examinate animal", ActionToDo = ExaminteReaction },
+               //new Menu.MenuItem { Label = "Show list of animals", ActionToDo =  }
+               new Menu.MenuItem { Label = "Remove animal", ActionToDo = RemoveAnimal }
+               new Menu.MenuItem { Label = "Quit", ActionToDo = null }
+            });
 
-            Console.WriteLine("[H] About company\n[R] Put new animal in clinic\n[E]Examinate animal\n[L] Show list of animals\n[D] Discharge animal\n[Q] Quit\n");
+            Console.WriteLine("[H] About company\n[R] Put new animal in clinic\n[E]Examinate animal\n[L] Show list of animals\n[D] Remove animal\n[Q] Quit\n");
             string menuChoise = "";
-
+            
             do
             {
                 menuChoise = Console.ReadLine();
 
-                if (menuChoise.ToUpper() == "H")
-                {
-                    Console.WriteLine("This is veterinary clinic for domestic animals:)");
-                }
-                else if (menuChoise.ToUpper() == "R")
-                {
-                    Console.WriteLine("Input animal name:");
-                    string nameOfAnimal = Console.ReadLine();
-                    Console.WriteLine("\nPlease, input age of animal");
-                    int ageOfAnimal;
-                    Int32.TryParse(Console.ReadLine(), out ageOfAnimal);
-                    int inputTypeOfanimal;
-                    Console.WriteLine("Input type of animal from the list below:\n");
-                    PrintEnumOfAnimalTypes();
-                    Int32.TryParse(Console.ReadLine(), out inputTypeOfanimal);
-                    DomesticAnimal newAnimal = null;
-                    switch (inputTypeOfanimal)
-                    {
-                        case 0:
-                            newAnimal = new Cat(nameOfAnimal, ageOfAnimal);
-                            break;
-                        case 1:
-                            newAnimal = new Dog(nameOfAnimal, ageOfAnimal);
-                            break;
-                        case 2:
-                            newAnimal = new Hamsted(nameOfAnimal, ageOfAnimal);
-                            break;
-                        case 3:
-                            newAnimal = new Fish(nameOfAnimal, ageOfAnimal);
-                            break;
-                        default:
-                            Console.WriteLine("Unfortunately, our clinic doesn't take care of such animals");
-                            break;
-                    }
-                    
-                    AnimalRegistry.AddAnimal(newAnimal);
-                }
-                else if (menuChoise.ToUpper() == "E")
-                {
-                    for (int i = 0; i < AnimalRegistry.Count; i++)
-                    {
-                        AnimalRegistry[i].ExamReaction();
-                    }
-                }
                 else if (menuChoise.ToUpper() == "L")
                 {
                     PrintListOfAnimals(AnimalRegistry);
                 }
                 else if (menuChoise.ToUpper() == "D")
                 {
-                    Console.WriteLine("Input number of animal:");
-                    int number;
-                    Int32.TryParse(Console.ReadLine(), out number);
-                    AnimalRegistry.RemoveAnimal(number);
+                    Console.WriteLine("Input animal's ID:");
+                    int ID;
+                    Int32.TryParse(Console.ReadLine(), out ID);
+                    AnimalRegistry.RemoveAnimal(ID);
                 }
             } while (menuChoise.ToUpper() != "Q");
 
@@ -104,6 +70,66 @@ namespace VeterinaryValeriya
             Console.WriteLine("<2> for {0}", TypeOfAnimal.Hamsted.ToString());
             Console.WriteLine("<3> for {0}", TypeOfAnimal.Fish.ToString());
             Console.WriteLine();
+        }
+
+        public char GetUserInput()
+        {
+            return Console.ReadLine()[0];
+        }
+
+        private static void ShowAboutCompany ()
+        {
+            Console.WriteLine("This is veterinary clinic for domestic animals:)");
+        }
+
+        private static void PutAnimalInClinic()
+        {
+            Console.WriteLine("Input animal name:");
+            string nameOfAnimal = Console.ReadLine();
+            Console.WriteLine("\nPlease, input age of animal");
+            int ageOfAnimal;
+            Int32.TryParse(Console.ReadLine(), out ageOfAnimal);
+            int inputTypeOfanimal;
+            Console.WriteLine("Input type of animal from the list below:\n");
+            PrintEnumOfAnimalTypes();
+            Int32.TryParse(Console.ReadLine(), out inputTypeOfanimal);
+            DomesticAnimal newAnimal = null;
+            switch (inputTypeOfanimal)
+            {
+                case 0:
+                    newAnimal = new Cat(nameOfAnimal, ageOfAnimal);
+                    break;
+                case 1:
+                    newAnimal = new Dog(nameOfAnimal, ageOfAnimal);
+                    break;
+                case 2:
+                    newAnimal = new Hamsted(nameOfAnimal, ageOfAnimal);
+                    break;
+                case 3:
+                    newAnimal = new Fish(nameOfAnimal, ageOfAnimal);
+                    break;
+                default:
+                    throw ApplicationException("Unfortunately, our clinic doesn't take care of such animals");
+                    break;
+            }
+
+           AnimalRegistry.AddAnimal(newAnimal);
+        }
+
+        private static void ExaminteReaction ()
+        {
+            for (int i = 0; i < AnimalRegistry.Count; i++)
+            {
+                AnimalRegistry[i].ExamReaction();
+            }
+        }
+
+        private static void RemoveAnimal ()
+        {
+            Console.WriteLine("Input animal's ID:");
+            int ID;
+            Int32.TryParse(Console.ReadLine(), out ID);
+            AnimalRegistry.RemoveAnimal(ID);
         }
     }
 }
